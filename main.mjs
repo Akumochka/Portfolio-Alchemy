e2f3f205caf7255138d2c477132fd53e1518ee25
Akumochka <evakochneva@gmail.com>
2026-03-11 15:14:03 +0100

    first commit

---
+++ main.mjs
@@ -0,0 +1,51 @@
+const baseURL = "https://alchemy-kd0l.onrender.com";
+const startURL = `${baseURL}/start`;
+const statusURL = `${baseURL}/status`;
+const submitURL = `${baseURL}/submit`;
+const clueURL = `${baseURL}/clue`;
+
+//Start
+const userConfig = { 
+    "email": "evgeniiae@uia.no", 
+    "nick": "Akuma", 
+    "pin": "8811"
+};
+
+let response = await (await fetch(startURL, {
+    body: JSON.stringify(userConfig), 
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    }
+})).json();
+
+console.log(response);
+
+//Token 
+let token = response.token;
+
+console.log(token);
+
+response = await (await fetch(statusURL,{
+    method: "GET",
+    headers: {
+        "Accept": "application/json",
+        "Authorization": token,
+        "Content-Type": "application/json"
+    }
+})).json();
+
+console.log(response);
+
+//Answer
+const answerResponse = await (await fetch(submitURL, {
+    method: "POST",
+    headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": token
+    },
+    body: JSON.stringify({ answer: "4" }) 
+})).json();
+
+console.log(answerResponse);
